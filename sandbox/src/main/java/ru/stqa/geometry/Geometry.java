d4ea69aed0ba0ed63d0b9b973d4ca62e599b3502
eva239 <239eva@gmail.com>
2025-10-28 20:12:36 +0300

    Выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,16 @@
+package ru.stqa.geometry;
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.PrintSquareArea(7.0);
+        Square.PrintSquareArea(5.0);
+        Square.PrintSquareArea(3.5);
+
+        Rectangle.printRectangleArea (3.0,5.0);
+        Rectangle.printRectangleArea (7.0,11.0);
+    }
+
+}
---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry.figures;
+
+public class Square {
+    public static void PrintSquareArea (double a) {
+        String text = String.format(
+                "Площадь квадрата со стороной %f = %f", a, squareArea(a));
+        System.out.println(text);
+
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
+    }
+}
